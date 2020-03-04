676d72c245bf0444f686b82b57dcbd0cd53d588a
atsuya-koshida <59789801+atsuya-koshida@users.noreply.github.com>
2020-03-04 09:11:54 +0900

    create tweet model, controller #index view (#2)

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,41 @@
+class TweetsController < ApplicationController
+  before_action :set_tweet, only: [:edit, :show]
+  
+  def index
+    @tweets = Tweet.where(user_id: current_user.id).order("created_at DESC")
+  end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      redirect_to root_path
+    else
+      # flash.now[:alert] = '出品に失敗しました'
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    tweet = tweet.find(params[:id])
+    tweet.update(tweet_params)
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:title, :content, images: []).merge(user_id: current_user_id)
+  end
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_03_233556) do
+ActiveRecord::Schema.define(version: 2020_03_03_234938) do
+
+  create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "title", null: false
+    t.string "text", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tweets_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +35,5 @@ ActiveRecord::Schema.define(version: 2020_03_03_233556) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "tweets", "users"
 end
