1f3d89fe14df5b9877589f733afa96d0e323fa88
atsuyakoshida <koshi0845@gmail.com>
2020-03-05 11:08:28 +0900

    image style fix

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,4 +3,6 @@ class Tweet < ApplicationRecord
   has_many_attached :images
 
   validates :text, presence: true
+  validates :text, length: { maximum: 140 }
+  validates :images, length: { maximum: 4 }
 end
