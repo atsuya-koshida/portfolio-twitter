8c035bdc61a2e6b0baa082b5ee8450085c33b608
atsuyakoshida <koshi0845@gmail.com>
2020-03-13 16:46:50 +0900

    fix user_image

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :like]
   def show
     @user = User.find(params[:id])
     @tweet = Tweet.new
@@ -6,6 +7,10 @@ class UsersController < ApplicationController
     @liked_tweets = @user.liked_tweets
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+  
   def like
     @user = User.find(params[:id])
     @liked_tweets = @user.liked_tweets
@@ -15,5 +20,9 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:nickname, :email, :image)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
